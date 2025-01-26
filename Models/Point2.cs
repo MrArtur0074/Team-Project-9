ee6cfc748854e227330378a63791863d84bf4b58
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2025-01-26 22:20:39 +0600

    feat: add `Point2` struct
    Introduced a new structure `Point2` to represent
    a two-dimensional point with x and y coordinates.

---
+++ Models/Point2.cs
@@ -0,0 +1,17 @@
+namespace Project_9.Models;
+
+public struct Point2
+{
+	public float X;
+	public float Y;
+
+	public Point2() {
+		X = 0f;
+		Y = 0f;
+	}
+	
+	public Point2(float x, float y) {
+		X = x;
+		Y = y;
+	}
+}
