a092a859f0cb2548f449bbffd8f11a24825d90d9
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2025-01-29 02:41:15 +0600

    feat(models): add `ITaperedWing` interface
    - Introduced `ITaperedWing` interface to represent a tapered wing model.
    - Includes properties for the root chord length and taper ratio to
      define the wing geometry.

---
+++ Models/Interfaces/ITaperedWing.cs
@@ -0,0 +1,18 @@
+namespace Project_9.Models.Interfaces;
+
+/// <summary>
+/// Represents a tapered wing model with a root chord and taper ratio.
+/// </summary>
+public interface ITaperedWing
+{
+	/// <summary>
+	/// Gets or sets the root chord length of the tapered wing in millimeters.
+	/// </summary>
+	public int RootChord { get; set; }
+	
+	/// <summary>
+	/// Gets or sets the taper ratio of the wing, which is
+	/// the ratio of the tip chord length to the root chord length.
+	/// </summary>
+	public float TaperRatio { get; set; }
+}
