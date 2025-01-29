b53f6bfd1950db338d5f9c8d4cbdc79eca6ad69f
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2025-01-30 00:51:31 +0600

    feat: add constructor to Airfoil class
    Added a constructor to initialize an Airfoil instance
    with a name and a set of points defining its shape.

--- Models/Airfoil.cs
+++ Models/Airfoil.cs
@@ -14,4 +14,16 @@ public class Airfoil
 	/// Gets or sets the points that define the shape of the airfoil.
 	/// </summary>
 	public Point2[] Points { get; set; }
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Airfoil"/> class with a specified name and set of points.
+	/// </summary>
+	/// <param name="name">The name of the airfoil.</param>
+	/// <param name="points">
+	/// An array of <see cref="Point2"/> representing the points defining the shape of the airfoil.
+	/// </param>
+	public Airfoil(string name, Point2[] points) {
+		Name = name;
+		Points = points;
+	}
 }
