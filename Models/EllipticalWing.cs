8c36af5f29f6b86bbf36cbd5212e8bc2e599aad8
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2025-02-01 18:21:28 +0600

    feat(models): add chord property to EllipticalWing
    - Introduced `Chord` property with validation logic to ensure the value
      remains within defined wing parameter constraints.
    - Updated the constructor to initialize the new `Chord` property.
    - Implements the `ISingleChordWing` interface.

--- Models/EllipticalWing.cs
+++ Models/EllipticalWing.cs
@@ -7,11 +7,27 @@ namespace Project_9.Models;
 /// Represents an elliptical wing, a specialized type of wing with 
 /// an elliptical planform for optimized aerodynamic performance.
 /// </summary>
-public class EllipticalWing : Wing, IEllipticalWing
+public class EllipticalWing : Wing, ISingleChordWing, IEllipticalWing
 {
+	private int   _chord;
 	private float _sweepCoefficient;
 	private float _tipExclusionRatio;
 	
+	/// <inheritdoc />
+	/// <exception cref="ArgumentOutOfRangeException">
+	/// Thrown when the chord length is out of the defined range
+	/// </exception>
+	public int Chord {
+		get => _chord;
+		set {
+			if (value is < WingParameters.MinRootChord or > WingParameters.MaxRootChord) {
+				throw new ArgumentOutOfRangeException(
+					$"Chord must be in range [{WingParameters.MinRootChord}; {WingParameters.MaxRootChord}]");
+			}
+			_chord = value;
+		}
+	}
+
 	/// <inheritdoc />
 	/// <exception cref="ArgumentOutOfRangeException">
 	/// Thrown when the sweep coefficient is out of the defined range.
@@ -48,11 +64,13 @@ public class EllipticalWing : Wing, IEllipticalWing
 	/// <summary>
 	/// Initializes a new instance of the <see cref="EllipticalWing"/> class with the specified parameters.
 	/// </summary>
+	/// <param name="chord">The length of the wing root chord.</param>>
 	/// <param name="sweep">The sweep coefficient, defining the curvature of the leading edge.</param>
 	/// <param name="tipExclusion">The ratio of the tip exclusion, limiting the rib generation area.</param>
 	public EllipticalWing(int span, float incidenceAngle, Airfoil rootAirfoil, Airfoil tipAirfoil, 
-		float sweep, int tipExclusion) 
+		int chord, float sweep, int tipExclusion) 
 		: base(span, incidenceAngle, rootAirfoil, tipAirfoil) {
+		Chord = chord;
 		SweepCoefficient = sweep;
 		TipExclusionRatio = tipExclusion;
 	}
