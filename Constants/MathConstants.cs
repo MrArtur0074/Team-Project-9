55641401fb56a87bb784d542019eddceb0f3a516
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2025-02-08 20:14:36 +0600

    feat(constants): add tolerance value constants
    - Defined `Tolerance` constant set to `1e-10` for precision comparisons.

--- Constants/MathConstants.cs
+++ Constants/MathConstants.cs
@@ -2,5 +2,9 @@ namespace Project_9.Constants;
 
 public class MathConstants
 {
-	
+	/// <summary>
+	/// The tolerance for floating point operations.
+	/// Values below this value are considered equal to 0.
+	/// </summary>
+	public const double Tolerance = 1e-10;
 }
