a4520452acd0a2cb48533ab5388803e169b30d93
tamiramil <tamiramil@proton.me>
2025-02-13 00:42:45 +0600

    feat(models): add rect and circle spar profiles
    - Introduced `RectSparProfile` and `CircleSparProfile` classes
      inheriting the abstract class `SparProfile`.
    - The classes represent the rectangular and circle spar profile types
      respectively.

---
+++ Models/RectSparProfile.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+
+namespace Project_9.Models;
+
+/// <summary>
+/// Represents a rectangular profile of wing spar.
+/// </summary>
+public class RectSparProfile : SparProfile
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="RectSparProfile"/> class with specified parameters.
+	/// </summary>
+	/// <param name="rectangle">The rectangle of the profile.</param>
+	public RectSparProfile(Rect rectangle) {
+		Rectangle = rectangle;
+	}
+	
+	public Rect Rectangle { get; set; }
+}
