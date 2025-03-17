134f9b1b7b7848b29ef12474599adf2a8a46d3bc
tamiramil <tamiramil@proton.me>
2025-03-17 21:29:40 +0600

    feat(config): add Config class for value retrieval
    Introduced new static `Config` class to retrieve values from the
    `Resources/config.json` file.

---
+++ Config/Config.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Project_9.Config;
+
+public static class Config
+{
+	private static readonly string ConfigPath =
+		Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "config.json");
+
+	public static string? GetValue(string key) {
+		try {
+			if (!File.Exists(ConfigPath)) {
+				throw new FileNotFoundException($"Configuration file not found: {ConfigPath}.");
+			}
+			string jsonContent = File.ReadAllText(ConfigPath);
+			var configData = JsonSerializer.Deserialize<JsonObject>(jsonContent);
+			if (configData is null || !configData.ContainsKey(key)) {
+				throw new KeyNotFoundException($"Key {key} not found in the configuration file.");
+			}
+			return configData[key].ToString();
+		} catch (Exception ex) {
+			Console.WriteLine($"Error reading config: {ex.Message}.");
+			return null;
+		}
+	}
+}
