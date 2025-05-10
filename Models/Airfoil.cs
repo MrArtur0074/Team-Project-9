cc22d4538a132892555e8e5e57cb4ca616407636
tamiramil <tamiramil@proton.me>
2025-05-10 16:15:06 +0600

    refactor: separate upper and lower airfoil points
    - Split mixed point array into distinct upper and lower surfaces to
      better reflect geometry and enable consistent ordering.
    - Added `GetPointsSelig` for Selig format reconstruction.

--- Models/Airfoil.cs
+++ Models/Airfoil.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Aspose.CAD.Primitives;
 
 namespace Project_9.Models;
@@ -13,19 +15,30 @@ public class Airfoil
 	public string Name { get; set; }
 
 	/// <summary>
-	/// Gets or sets the points that define the shape of the airfoil.
+	/// Gets or sets the points that define the upper surface of the airfoil.
 	/// </summary>
-	public (Point2D point, bool isUpper)[] Points { get; set; }
+	public Point2D[] UpperPoints { get; set; }
+
+	/// <summary>
+	/// Gets or sets the points that define the lower surface of the airfoil.
+	/// </summary>
+	public Point2D[] LowerPoints { get; set; }
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="Airfoil"/> class with a specified name and set of points.
 	/// </summary>
 	/// <param name="name">The name of the airfoil.</param>
-	/// <param name="points">
-	/// The array of <see cref="Point2"/> representing the points defining the shape of the airfoil.
-	/// </param>
-	public Airfoil(string name, (Point2D, bool)[] points) {
+	/// <param name="upperPoints">The array of points defining the upper surface of the airfoil.</param>
+	/// <param name="lowerPoints">The array of points defining the lower surface of the airfoil.</param>
+	public Airfoil(string name, Point2D[] upperPoints, Point2D[] lowerPoints) {
 		Name = name;
-		Points = points;
+		UpperPoints = upperPoints;
+		LowerPoints = lowerPoints;
+	}
+
+	public Point2D[] GetPointsSelig() {
+		var upperSurface = UpperPoints.OrderByDescending(p => p.X).ToArray();
+		var lowerSurface = LowerPoints.Skip(1).OrderBy(p => p.X).ToArray();
+		return upperSurface.Concat(lowerSurface).ToArray();
 	}
 }
