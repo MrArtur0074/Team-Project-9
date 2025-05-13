f5dfbd3bd2194469226e49afe3596e2ec01f5699
tamiramil <tamiramil@proton.me>
2025-05-13 22:06:04 +0600

    feat: support JSON-to-DXF wing export via CLI
    - Introduces CLI interface that parses a wing from JSON, generates ribs,
      and exports to DXF.
    - Handles invalid input and reports success or errors to the console.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using Project_9.Models;
+﻿using System;
+using System.IO;
+using System.Linq;
+using netDxf.Header;
 using Project_9.Services;
 
 namespace Project_9;
@@ -6,6 +9,28 @@ namespace Project_9;
 public static class Program
 {
 	public static void Main(string[] args) {
+		if (args.Length == 0) {
+			Console.WriteLine("Provide path to JSON configuration file");
+			return;
+		}
 
+		try {
+			var wing = new WingJsonParserService().ParseWingFromJson(args[0]);
+			var blocks = new RibsInterpolationService().Interpolate(wing).ToList();
+
+			var result = DxfExportService.Export(
+				Path.ChangeExtension(args[0], ".dxf"),
+				blocks,
+				DxfVersion.AutoCad2018,
+				true);
+
+			Console.WriteLine(!result.IsSuccess
+				? $"Error: {result.ErrorMessage}"
+				: "Wing successfully generated!"
+			);
+		}
+		catch (Exception ex) {
+			Console.WriteLine($"Error: {ex.Message}");
+		}
 	}
 }
