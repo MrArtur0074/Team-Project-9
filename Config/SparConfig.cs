645e4d88315275e3b815f9996bcabe4f0f30ed80
tamiramil <tamiramil@proton.me>
2025-05-13 21:27:56 +0600

    feat(parser): add JSON config support for wings
    - Introduces WingJsonParserService to parse wing geometry and spar
      structure from JSON files.
    - Enables external configuration of wing parameters, supporting
      straight, tapered, and elliptical types.

---
+++ Config/SparConfig.cs
@@ -0,0 +1,16 @@
+namespace Project_9.Parsers;
+
+public class SparConfig
+{
+	public string Type { get; set; }
+	public int StartRib { get; set; }
+	public int EndRib { get; set; }
+	public double StartChordOffset { get; set; }
+	public double EndChordOffset { get; set; }
+	public string Alignment { get; set; }
+	public double YOffset { get; set; }
+	public double Radius { get; set; }
+	public double Width { get; set; }
+	public double Height { get; set; }
+	public string ProfileAlignment { get; set; }
+}
---
+++ Services/WingJsonParserService.cs
@@ -0,0 +1,88 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using Project_9.Models;
+using Project_9.Parsers;
+
+namespace Project_9.Services;
+
+public class WingJsonParserService
+{
+	public Wing ParseWingFromJson(string jsonFilePath) {
+		if (!File.Exists(jsonFilePath)) {
+			throw new FileNotFoundException("JSON configuration file not found", jsonFilePath);
+		}
+
+		string jsonContent = File.ReadAllText(jsonFilePath);
+		var config = JsonSerializer.Deserialize<WingConfig>(jsonContent);
+
+		var rootAirfoil = AirfoilParserService.Parse(config.RootAirfoilPath);
+		var tipAirfoil = AirfoilParserService.Parse(config.TipAirfoilPath);
+
+		Wing wing = config.Type.ToLower() switch {
+			"straight" => new StraightWing(
+				config.Name,
+				config.RootChord,
+				config.Span,
+				config.IncidenceAngle,
+				rootAirfoil,
+				tipAirfoil),
+
+			"tapered" => new TaperedWing(
+				config.Name,
+				config.RootChord,
+				config.Span,
+				config.IncidenceAngle,
+				rootAirfoil,
+				tipAirfoil,
+				config.TaperRatio),
+
+			"elliptical" => new EllipticalWing(
+				config.Name,
+				config.RootChord,
+				config.Span,
+				config.IncidenceAngle,
+				rootAirfoil,
+				tipAirfoil,
+				config.Sweep,
+				config.TipExclusion),
+
+			_ => throw new ArgumentException($"Unknown wing type: {config.Type}")
+		};
+
+		wing.Ribs.ResetWithRibsNumber(config.RibCount);
+
+		foreach (var sparConfig in config.Spars) {
+			Spar spar = sparConfig.Type.ToLower() switch {
+				"circle" => new CircleSpar(
+					config.RibCount,
+					sparConfig.StartRib,
+					sparConfig.EndRib,
+					sparConfig.StartChordOffset,
+					sparConfig.EndChordOffset,
+					Enum.Parse<Spar.AlignmentType>(sparConfig.Alignment),
+					sparConfig.YOffset,
+					sparConfig.Radius),
+
+				"rect" => new RectSpar(
+					config.RibCount,
+					sparConfig.StartRib,
+					sparConfig.EndRib,
+					sparConfig.StartChordOffset,
+					sparConfig.EndChordOffset,
+					Enum.Parse<Spar.AlignmentType>(sparConfig.Alignment),
+					sparConfig.YOffset,
+					sparConfig.Height,
+					sparConfig.Width) {
+					ProfileAlignment = Enum.Parse<RectSpar.ProfileAlignmentType>(sparConfig.ProfileAlignment)
+				},
+
+				_ => throw new ArgumentException($"Unknown spar type: {sparConfig.Type}")
+			};
+
+			wing.Spars.Add(spar);
+		}
+
+		return wing;
+	}
+}
